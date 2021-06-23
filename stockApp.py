83daa5cd6f79bbb52a07e018593bde8188589fd3
danOr266 <53933171+danOr266@users.noreply.github.com>
2021-06-23 16:23:09 +0200

    Create stockApp.py

---
+++ stockApp.py
@@ -0,0 +1,16 @@
+
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+""")
+
+tickerSymbol = 'GOOGL'
+tickerData =  yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(period = 'id', start = '2010-05-01', end = '2021-06-01')
+tickerDf
+
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
