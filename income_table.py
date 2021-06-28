9233b6d64b14626afed20332d47f6c24ee6fd5be
Orwa, Daniel Rainer <dorwa@deloitte.de>
2021-06-28 06:24:59 +0200

    Update income_table.py

--- income_table.py
+++ income_table.py
@@ -6,7 +6,12 @@ import datetime
 from collections import OrderedDict
 from dateutil.relativedelta import *
 
-
+def BSV_indicator_payment(x):
+    if x <= 10 :
+        BVS_payment_indicator = 1
+    else :
+        BVS_payment_indicator = 0
+    return BVS_payment_indicator
 
 def income_table(start_date, income_p1, income_p2, income_increase_rate ,bonus = 0, bonus_month = '12', no_kids = 1, kindergeld_rate = 219, base_living_expenses = 2700, 
                 living_cost_inflation= 1.02,no_projection_years = 22, BSV_ind = 1, BSV_extra = 300):
@@ -18,12 +23,14 @@ def income_table(start_date, income_p1, income_p2, income_increase_rate ,bonus =
 
     start_year = start_date.year
 
+   
+
     for i in df.index:
        # if i < datetime.datetime(2022, 7, 1):
         df.loc[df.index == i,'income_p2'] = income_p2*pow(income_increase_rate, i.year -start_year) +  BSV_extra
         df.loc[df.index == i,'income_p1'] = income_p1*pow(income_increase_rate, i.year -start_year) +  BSV_extra
         df.loc[df.index == i,'Kindergeld'] = no_kids * kindergeld_rate
-        df.loc[df.index == i,'living_exp'] =  (base_living_expenses )*pow(living_cost_inflation, i.year -start_year) +  BSV_extra
+        df.loc[df.index == i,'living_exp'] =  (base_living_expenses )*pow(living_cost_inflation, i.year -start_year) +  BSV_extra*BSV_ind*BVS_payment_indicator(i.year -start_year)
  
   #  df.loc[df.index == datetime.datetime(2025, 7, 1),'income_p1'] = df[df.index ==datetime.datetime(2025, 7, 1)]['income_p1'][0] +37000
 
