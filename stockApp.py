6d322538605a67051f3cbde8dc7e45e6dd62a885
Orwa, Daniel Rainer <dorwa@deloitte.de>
2021-06-28 18:23:07 +0200

    Update stockApp.py

--- stockApp.py
+++ stockApp.py
@@ -30,11 +30,11 @@ i = 1
 for ZB in ZB_to_compare:
      with st.form(key=f'my_form{ZB}'):
           st.write(f'For the{ZB}, enter the following load details')
-          {ZB} = st.number_input(label='Enter the loan amount to be borrowed')
-          years = st.number_input(label='Enter the loan repayment length in years')
-          payments_year = st.number_input(label='Enter the number of payments in a year')
-          start_date = st.sidebar.date_input(label = 'Selection income start projection date', value =None, min_value = date.today())
-          submit_button = st.form_submit_button(label='Submit')
+          f'loan_amount{ZB}' = st.number_input(label='Enter the loan amount to be borrowed')
+          f'years{ZB}' = st.number_input(label='Enter the loan repayment length in years')
+          f'payments_year{ZB}' = st.number_input(label='Enter the number of payments in a year')
+          f'start_date{ZB}' = st.sidebar.date_input(label = 'Selection income start projection date', value =None, min_value = date.today(), )
+          f'submit_button{ZB}' = st.form_submit_button(label='Submit')
 
 
 st.write([{ZB}, payments_year])
