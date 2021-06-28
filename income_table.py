bb6ff982bec3b9c632e9c68ab3e901509191e0dd
Orwa, Daniel Rainer <dorwa@deloitte.de>
2021-06-28 15:01:09 +0200

    Update income_table.py

--- income_table.py
+++ income_table.py
@@ -23,8 +23,6 @@ def income_table(start_date, income_p1, income_p2, income_increase_rate ,bonus =
 
     start_year = start_date.year
 
-   
-
     for i in df.index:
        # if i < datetime.datetime(2022, 7, 1):
         df.loc[df.index == i,'income_p2'] = income_p2*pow(income_increase_rate, i.year -start_year) 
@@ -32,8 +30,6 @@ def income_table(start_date, income_p1, income_p2, income_increase_rate ,bonus =
         df.loc[df.index == i,'Kindergeld'] = no_kids * kindergeld_rate
         df.loc[df.index == i,'living_exp'] =  (base_living_expenses )*pow(living_cost_inflation, i.year -start_year) +  BSV_extra*BSV_ind*BSV_indicator_payment(i.year -start_year)
  
-  #  df.loc[df.index == datetime.datetime(2025, 7, 1),'income_p1'] = df[df.index ==datetime.datetime(2025, 7, 1)]['income_p1'][0] +37000
-
     for i in df.index:
         if i.strftime("%m") == bonus_month:
             df.loc[df.index == i,'bonus'] = bonus
