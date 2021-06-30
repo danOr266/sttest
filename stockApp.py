7367708ab3fd83dc488851ce52b300049ee5ccda
Orwa, Daniel Rainer <dorwa@deloitte.de>
2021-06-30 19:35:02 +0200

    Update stockApp.py

--- stockApp.py
+++ stockApp.py
@@ -14,6 +14,7 @@ import scenario_generation
 import other_function as of
 import decimal as dl
 import input_columns
+import income_input
 
 #Inc_Exp = st.sidebar.file_uploader('File uploader', type=["csv"]   ) #Inc_Exp = pd.read_csv(Inc_Exp)
 st.set_page_config(layout="wide")
@@ -23,33 +24,34 @@ st.sidebar.subheader('Input - Scenarios to run')
 ZB_array = np.array([10, 15, 20])
 BSV_ind_array = np.array([0, 1])
 
-
-start_date = st.sidebar.date_input(label = 'Selection income start projection date1', 
-                                   value =None, min_value = date.today())
-ZB_to_compare = st.sidebar.multiselect(
-     'Select the Zinsbindung scenarios you would like to compare?',
-     ZB_array )
-
-BSV_to_compare = st.sidebar.radio(
-     'Select wheather you wish to use BSV?', BSV_ind_array )
+#Getting Main input from the user
+loan_amount = st.sidebar.number_input(label='Enter the loan amount to be borrowed')
+start_date = st.sidebar.date_input(label = 'Selection income start projection date1', value =None, min_value = date.today())
+ZB_to_compare = st.sidebar.multiselect( 'Select the Zinsbindung scenarios you would like to compare?', ZB_array )
+BSV_to_compare = st.sidebar.radio( 'Select wheather you wish to use BSV?', BSV_ind_array )
+scenarios_to_compare = st.sidebar.slider('select range of Interest rate shock',min_value = -0.5, max_value=5.0, value=(0.0,3.0), step=0.5)
 BSV_to_compare = int(BSV_to_compare)
 
-if BSV_to_compare == 0:
-     BSV_amount = 0
-     BSV_ind = 0
-     BSV_loan_amount = 0
-else:
+if BSV_to_compare == 1:
      BSV_ind = 1
      BSV_amount = st.sidebar.number_input(label='Enter the BSV accumulated amount + loan amount to be borrowed')
      BSV_loan_amount =  st.sidebar.number_input(label='Enter the BSV  loan amount to be repayed')
+else:
+     BSV_amount = 0
+     BSV_ind = 0
+     BSV_loan_amount = 0
 
-scenarios_to_compare = st.sidebar.slider('select range of Interest rate shock',min_value = -0.5, max_value=5.0, value=(0.0,3.0), step=0.5)
+scenario_vector = None
 
-scenario_vector = np.array(list(of.drange(scenarios_to_compare[0],scenarios_to_compare[1],jump= 0.5)))*0.01
+if scenario_vector == None:
+     st.stop()
+else :
+     scenario_vector = np.array(list(of.drange(scenarios_to_compare[0],scenarios_to_compare[1],jump= 0.5)))*0.01
 
 with st.form(key = 'input_column_form'):
+     st.subheader('Loan Input Data')
      st.form_submit_button(label='Input loan Details',key = 'inputloanndetails_key')
-     scenario_df = input_columns.input_columns(start_date, ZB_to_compare, BSV_to_compare, BSV_ind, BSV_amount, BSV_loan_amount)
+     scenario_df = input_columns.input_columns(start_date, loan_amount ,ZB_to_compare, BSV_to_compare, BSV_ind, BSV_amount, BSV_loan_amount)
      if st.button('Input Loan Details', key = 'input_loan_details_key'):
           scenario_graphic1, scenario_graphic2, scenario_graphic3  = st.beta_columns(3)
           scenario_vector1 = pd.DataFrame(scenario_vector, columns=['Interest_increase'])
@@ -61,10 +63,13 @@ with st.form(key = 'input_column_form'):
                st.write(scenario_df)
     
 
-st.sidebar.subheader('Income Projection Input Data')
+if scenario_df is not None :
+     st.subheader('Income Projection Input Data')
+     income_input_dic = income_input.income_input()
+     st.write(income_input_dic)
 
 
-
+st.stop()
 #income_projection_start_date = income_projection_start_date.datetime.date()
 
 if st.button('Income Projection',  key = 'Income_Projection_key'):
